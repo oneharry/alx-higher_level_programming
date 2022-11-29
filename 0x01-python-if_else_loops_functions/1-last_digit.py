89dab39d4ed6357d86dd1444df0c9593d6f38c8b
oneharry <harryofficialchidi@yahoo.com>
2022-11-29 19:16:40 +0000

    added tast 1 printing last digit of numb

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastdig = abs(number) % 10
+    print("Last digit of {:d} is -{:d} and\
+ is less than 6 and not 0".format(number, lastdig))
+else:
+    lastdig = abs(number) % 10
+    if lastdig > 5:
+        print("Last digit of {:d} is {:d}\
+ and is greater than 5".format(number, lastdig))
+    elif lastdig == 0:
+        print("Last digit of {:d} is {:d} and is 0".format(number, lastdig))
+    else:
+        print("Last digit of {:d} is {:d} and\
+ is less than 6 and not 0".format(number, lastdig))
