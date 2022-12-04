133c24962e42046eb383fea5456801c6fedd95e2
oneharry <harryofficialchidi@yahoo.com>
2022-12-04 00:29:15 +0000

    max number in  alist

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     max = my_list[0]
     if my_list == []:
         return None
+
     for i in range(len(my_list)):
-        if my_list[i] > max:
+        if my_list[i] >= max:
             max = my_list[i]
     return max
