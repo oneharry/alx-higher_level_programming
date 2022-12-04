48c4700910ff664a3f42c47cdc97899573e4b0e1
oneharry <harryofficialchidi@yahoo.com>
2022-12-04 23:17:46 +0000

    added palindrome list check

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,6 +2,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+  * freeptr - frees memory alocc pointers
+  * @p: pointer arg
+  * Return: void
+  */
+void freeptr(int *p)
+{
+	free(p);
+}
 /**
   * is_palindrome - checks if a linked list is a palindrome
   * @head: pointer to the pointer of the list head
@@ -10,12 +20,14 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *thead = *head;
-	int arrlen, i = 0;
+	int arrlen, x, i = 0;
 	int *valptr;
 
-	if (thead == NULL)
-		return (0);
+	if (*head == NULL)
+		return (1);
 	valptr = malloc(1 * sizeof(int));
+	if (valptr == NULL)
+		return (0);
 	valptr[i] = thead->n;
 	while (thead->next)
 	{
@@ -27,16 +39,16 @@ int is_palindrome(listint_t **head)
 	for (i = 0; valptr[i]; i++)
 	{}
 	arrlen = i - 2;
-	for (i = 0; i <= arrlen;)
+	for (x = 0; x <= arrlen;)
 	{
-		if (valptr[i++] == valptr[arrlen--])
+		if (valptr[x++] == valptr[arrlen--])
 			continue;
 		else
 		{
-			free(valptr);
+			freeptr(valptr);
 			return (0);
 		}
 	}
-	free(valptr);
+	freeptr(valptr);
 	return (1);
 }
