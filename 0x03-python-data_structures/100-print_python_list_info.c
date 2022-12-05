ace826763d464fdd5b1df8dd5ae6dca32875bc3f
oneharry <harryofficialchidi@yahoo.com>
2022-12-05 22:33:02 +0000

    printing pyobject info

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <Python.h>
+
+/**
+  * print_python_list_info - print basic info about pyobject
+  * @p: pointer to pyobjecy\
+  * Return: void
+  */
+void print_python_list_info(PyObject *p)
+{
+
+}
