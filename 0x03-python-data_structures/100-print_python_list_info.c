aab2b0d6cd6f220a54deaa8844528cd7e0a1a36a
oneharry <harryofficialchidi@yahoo.com>
2022-12-06 20:31:15 +0000

    advanced task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,11 +1,25 @@
 #include <Python.h>
-
 /**
   * print_python_list_info - print basic info about pyobject
-  * @p: pointer to pyobjecy\
+  * @p: pointer to pyobject
   * Return: void
   */
 void print_python_list_info(PyObject *p)
 {
+	int size, mem, x;
+	PyObject *obj;
 
+	size = PY_SIZE(p);
+	mem = (p)->allocated;
+
+	printf("[*} Size of the Python List = %d\n", size);
+	printf("[*} Allocated = %d\n", mem);
+
+	for (x = 0, x < size; x++)
+	{
+		printf("Element %d: ", x);
+
+		obj = PyList_GetItem(p, x);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
 }
