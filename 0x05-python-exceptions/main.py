97dc9547a410311808eeb28505d858fade2d350b
oneharry <harryofficialchidi@yahoo.com>
2022-12-19 23:13:03 +0000

    kuk

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+safe_function = __import__('101-safe_function').safe_function
 
-try:
-    raise_exception_msg("C is fun")
-except NameError as ne:
-    print(ne)
+
+def my_div(a, b):
+    return a / b
+
+result = safe_function(my_div, 10, 2)
+print("result of my_div: {}".format(result))
+
+result = safe_function(my_div, 10, 0)
+print("result of my_div: {}".format(result))
+
+
+def print_list(my_list, len):
+    i = 0
+    while i < len:
+        print(my_list[i])
+        i += 1
+    return len
+
+result = safe_function(print_list, [1, 2, 3, 4], 10)
+print("result of print_list: {}".format(result))
