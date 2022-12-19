debeb0125a3ab72b9424ea2c6a02b594cef8f70d
oneharry <harryofficialchidi@yahoo.com>
2022-12-19 22:06:04 +0000

    test

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
     try:
         res = a / b
     except (TypeError, ZeroDivisionError):
         res = None
     finally:
         print("Inside result: {}".format(res))
+    return res
