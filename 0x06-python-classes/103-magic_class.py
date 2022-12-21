b8fa2d1f4cedbb32f7eab389aa0c6ece0ce903ce
oneharry <harryofficialchidi@yahoo.com>
2022-12-21 23:53:19 +0000

    magic

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -15,8 +15,8 @@ class MagicClass:
 
     def area(self):
         """ calculates the area """
-        return self.__radius ** math.pi
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """ calculates the circumference """
-        return math.pi * self.__radius
+        return (2 * math.pi) * self.__radius
