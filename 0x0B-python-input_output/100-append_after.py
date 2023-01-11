57582e135896e96802c7ae1de0f51ed049898de4
oneharry <harryofficialchidi@yahoo.com>
2023-01-11 00:54:29 +0000

    h

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+"""Modules to be worked on tomorrow """
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Modules creates a list of lists of integers represnting Paschals tr"""
+
+
+def pascal_triangle(n):
+    """
+        Functions returns list of lists of integers rep Paschals triangle
+        Args:
+            n: number of tri
+    """
