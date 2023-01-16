15bdef70625f7b4228d411288a9b22b6365afc49
oneharry <harryofficialchidi@yahoo.com>
2023-01-16 20:42:51 +0000

    fixing task1

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,1 +1,5 @@
-# Almost a circle 
+# Almost a circle
+## CLasses
+### Three classes Base, Rectangle and Square
+The Base class is the base class upon which the other 2 classes are defined.
+The 2 sub classes inherit attributes and methods from the base
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -4,16 +4,3 @@ import unittest
 from unittest.mock import patch
 from io import StringIO
 from models.square import Square
-
-
-class TestSquare(unittest.TestCase):
-    def test_area(self):
-        self.a = Square(2)
-        self.assertEqual(self.a.area, 4)
-
-    @patch('sys.stdout', new_callable=StringIO)
-    def test_display(self):
-        self.a = Square(4)
-        disp = "####\n####\n####\n####\n"
-        self.a.display()
-        self.assertEqual(stdout.getvalue(), disp)
