337915a323d15a7d2c7705244a6711b85d19c087
oneharry <harryofficialchidi@yahoo.com>
2023-02-17 12:09:32 +0000

    more chngs

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Lists all the cities of california in the DB "hbtn_0d_usa"
 SELECT id, name
   FROM cities
- WHERE states.id IN (
+ WHERE cities.id IN (
        SELECT id
          FROM states
 	WHERE name = 'California')
