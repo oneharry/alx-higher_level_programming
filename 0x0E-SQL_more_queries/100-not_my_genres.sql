c25f52c131a3430ee41c1539b7e319ad58c2f253
oneharry <harryofficialchidi@yahoo.com>
2023-02-17 14:49:35 +0000

    hu

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,12 +2,12 @@
 -- Lists all genres not linked to the show
 SELECT name 
   FROM tv_genres
-  WHERE name NOT IN (
+ WHERE name NOT IN (
        SELECT name
-         FROM tv_genres
-        INNER JOIN tv_show_genres
-	   ON tv_genres.id = tv_show_genres.genre_id
-        INNER JOIN tv_shows
-	   ON tv_show_genres.show_id = tv_shows.id
-	WHERE title = 'Dexter');
-
+         FROM tv_genres AS g
+        INNER JOIN tv_show_genres AS sh
+	   ON g.id = sh.genre_id
+        INNER JOIN tv_shows AS s
+	   ON sh.show_id = s.id
+	WHERE title = 'Dexter')
+ ORDER BY name;
