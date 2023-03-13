c4932acf7c46f90de1d63a492331dd3b28054d3a
oneharry <harryofficialchidi@yahoo.com>
2023-03-13 20:26:11 +0000

    firs

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* print 3 lines */
+const firstLine = 'C is fun';
+const secLine = 'Python is cool';
+const myVar = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secLine);
+console.log(myVar);
