5e8d2b711e3b8ab7aa76c617fcf25ce56abdfcf9
oneharry <harryofficialchidi@yahoo.com>
2023-03-13 22:51:26 +0000

    jdhd

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,9 +6,9 @@ if (!num || isNaN(num)) {
   console.log('Missing size');
 } else if (num > 0) {
   for (i = 0; i < num; i++) {
-	  let space = '';
+    let space = '';
     for (j = 0; j < num; j++) {
-	    space += 'X';
+      space += 'X';
     }
     console.log(space);
   }
