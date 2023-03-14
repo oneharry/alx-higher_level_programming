9f9dc9154297944ae2bef07235b0e9ae0d55fc31
oneharry <harryofficialchidi@yahoo.com>
2023-03-14 19:24:38 +0000

    jgj

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creates a user "user_0d_1" with password 'user_0d_1_pwd'
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
    ON * . *
-   TO 'user_0d_1'@'%' 
+   TO 'user_0d_1'@'localhost' 
    WITH GRANT OPTION;
