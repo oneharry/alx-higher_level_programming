34e67497ca7e91c2d09bcc64d194e80857e01ef8
oneharry <harryofficialchidi@yahoo.com>
2023-03-14 22:32:49 +0000

    l

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
-
-class Square extends Rectangle {
+class Square extends require('./4-rectangle.js') {
   constructor (size) {
     super(size, size);
   }
