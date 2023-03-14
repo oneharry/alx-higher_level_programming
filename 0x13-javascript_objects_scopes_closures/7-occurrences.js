10c2dd8146973b73d509a4864c83d8c161e8a88d
oneharry <harryofficialchidi@yahoo.com>
2023-03-14 22:48:50 +0000

    h

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,25 +1,10 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    let cha = 'X';
-    let i, j;
-    const size = this.height;
-    if (c) {
-      cha = c;
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((x, y) => {
+    if (y === searchElement) {
+      return x + 1;
+    } else {
+      return x;
     }
-    for (i = 0; i < size; i++) {
-      let space = '';
-      for (j = 0; j < size; j++) {
-        space += cha;
-      }
-      console.log(space);
-    }
-  }
-}
-module.exports = Square;
+  }, 0);
+};
