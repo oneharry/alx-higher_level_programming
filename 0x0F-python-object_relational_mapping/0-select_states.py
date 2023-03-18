8a888e893edf05fd52cc7b3c3611febdac45b7df
oneharry <harryofficialchidi@yahoo.com>
2023-03-18 13:45:49 +0000

    DBAPI select all states in db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """ Module prints all states in database"""
 
-
+import sys
 import MySQLdb
 
+arg = sys.argv
+conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
+                       passwd=arg[2], db=arg[3], charset="utf8")
+curr = conn.cursor()
+curr.execute("SLECT * FROM states ORDER BY states.id ASC")
+rows = curr.fetchall()
+for row in rows:
+    print(row)
+curr.close()
+conn.close()
