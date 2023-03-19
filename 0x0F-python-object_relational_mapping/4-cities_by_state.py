cd504a1f630369291d9e3fd9a0dc389a31939f99
oneharry <harryofficialchidi@yahoo.com>
2023-03-19 16:32:18 +0000

    fixed task 4 bugs

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module prints all cities in database
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    arg = sys.argv
+    conn = MySQLdb.connect(user=arg[1], passwd=arg[2], db=arg[3])
+    curr = conn.cursor()
+    curr.execute("SELECT DISTINCT cities.id, cities.name, states.name\
+                 FROM cities\
+                 INNER JOIN states\
+                 ON cities.id = states.id\
+                 ORDER BY cities.id")
+    for i in curr.fetchall():
+        print(i)
