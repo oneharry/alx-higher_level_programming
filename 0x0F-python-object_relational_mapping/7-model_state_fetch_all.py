307aa80a088780c038a9f2138ab59b5bdf72c42f
oneharry <harryofficialchidi@yahoo.com>
2023-03-20 00:05:02 +0000

    task 7: Lists all objects from db

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module definition of a class State
+    cities of that state
+"""
+
+from sqlalchemy import create_engine, sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ = "__main__":
+    db_url = "mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1],
+                                                         sys.argv[2],
+                                                         sys.argv[3])
+    engine = create_engine(db_url, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for s in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
