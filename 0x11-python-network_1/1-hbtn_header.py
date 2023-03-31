f65710096b683f7206c4d85b932d4d22f28946d4
oneharry <harryofficialchidi@yahoo.com>
2023-03-31 15:45:19 +0000

    got XRequest id value of the header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,14 +3,12 @@
 
 
 import urllib.request
+import sys
 if __name__ == '__main__':
     try:
-        with urllib.request.urlopen("https: // alx-intranet.hbtn.io /
-                                    status") as res:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as res:
             response = res.read()
-            print("Body response:")
-            print("\t- {}: {}".format("type", type(response)))
-            print("\t- {}: {}".format("content", response))
-            print("\t- {}: {}".format("utf8 content", res.msg))
+            print("{}".format(res.headers["X-Request-Id"]))
     except URLError as err:
         pass
