28370101997b779ae1edd06acf724daacb0c4a51
oneharry <harryofficialchidi@yahoo.com>
2023-03-31 15:40:01 +0000

    find status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,10 @@
 import urllib.request
 if __name__ == '__main__':
     try:
-        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-        as res:
+        url = "https://alx-intranet.hbtn.io/status"
+        with urllib.request.urlopen(url) as res:
             response = res.read()
+            print("Body response:")
             print("\t - {}: {}".format("type", type(response)))
             print("\t - {}: {}".format("content", response))
             print("\t - {}: {}".format("utf8 content", res.msg))
---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Fetches from URL = https://alx-intranet.hbtn.io/status """
+
+
+import urllib.request
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen("https: // alx-intranet.hbtn.io /
+                                    status") as res:
+            response = res.read()
+            print("Body response:")
+            print("\t- {}: {}".format("type", type(response)))
+            print("\t- {}: {}".format("content", response))
+            print("\t- {}: {}".format("utf8 content", res.msg))
+    except URLError as err:
+        pass
