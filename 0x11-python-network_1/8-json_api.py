d8f9e82e967700205732e3b715afb1001a17cb8b
oneharry <harryofficialchidi@yahoo.com>
2023-04-01 23:30:40 +0000

    Json API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Send POST request to http://0.0.0.0:5000/search_user with letter as a param
 displays id and name of body
 """
@@ -7,11 +7,19 @@ displays id and name of body
 
 import requests
 import sys
+import json
 if __name__ == '__main__':
     val = ""
-    if sys.argv[1]:
+    try:
+        if len(sys.argv) < 1 or type(eval(sys.argv[1])) != str:
+            raise IndexError
         val = sys.argv[1]
-    data = {'q': val}
-    res = requests.get('http://0.0.0.0:5000/search_user', data)
-    
-    
+        data = {'q': val}
+        res = requests.post('http://0.0.0.0:5000/search_user', data)
+        if json.load(res.text):
+            res.raise_for_status()
+        print(res.text)
+    except json.decoder.JSONDecodeError as err:
+        print("Not a valid JSON")
+    except IndexError:
+        print("No result")
