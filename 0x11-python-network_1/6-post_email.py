757fb741f894bde0491e1a8abbcd9a1d37f23672
oneharry <harryofficialchidi@yahoo.com>
2023-04-01 14:00:33 +0000

    Fixed post email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@
 import requests
 import sys
 if __name__ == '__main__':
-    res = requests.get(sys.argv[1], {'email': sys.argv[2]})
+    res = requests.post(sys.argv[1], data={'email': sys.argv[2]})
     print("{}".format(res.text))
