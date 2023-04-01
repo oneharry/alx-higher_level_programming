0836f770c80adffcef6fa2367aa63c6e3a3c3e93
oneharry <harryofficialchidi@yahoo.com>
2023-04-01 12:41:27 +0000

    fixed bug in status task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,5 +6,5 @@ import requests
 if __name__ == '__main__':
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(res.content)))
+    print("\t- type: {}".format(type(res.text)))
     print("\t- content: {}".format(res.text))
