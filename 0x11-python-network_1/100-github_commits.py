11ee3c06854f967a995a11c1ef83ea466da36425
oneharry <harryofficialchidi@yahoo.com>
2023-04-02 18:47:24 +0000

    Interview challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-""" Using github username and password """
+""" Solve interview challenge: Please list 10 commits
+(from the most recent to oldest)
+of the repository “rails” by the user “rails”
+"""
+
+
+import sys
+import requests
+if __name__ == '__main__':
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    header = {
+        "Accept": "application/vnd.github+json",
+        # "Authorization": "Bearer <YOUR-TOKEN>",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+    res = requests.get('https://api.github.com/repos/{owner}/{repo}/commits',
+                       headers=header)
+    r = res.json()
+    print("{}: {}".format(r.get("sha"), r.get("commit").get("author")
+          .get("name")))
