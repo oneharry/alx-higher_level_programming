02089ae0fbe1fe25ddfb4df99c95e145d073b33d
oneharry <harryofficialchidi@yahoo.com>
2023-04-02 17:34:48 +0000

    access with github api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 """ Using github username and password """
+
+
+import requests
+import sys
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    header = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+    res = requests.get(f'https://api.github.com/users/{username}',
+                       headers=header)
+    r = res.json()
+    print("{}".format(r.get("id")))
