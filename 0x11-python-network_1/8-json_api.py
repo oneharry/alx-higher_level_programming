58a1171d1667c0f7eba4fc9a29ff09893df3aae0
oneharry <harryofficialchidi@yahoo.com>
2023-04-02 00:13:12 +0000

    Json search api task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,19 +7,17 @@ displays id and name of body
 
 import requests
 import sys
-import json
 if __name__ == '__main__':
     val = ""
-    try:
-        if len(sys.argv) < 1 or type(eval(sys.argv[1])) != str:
-            raise IndexError
+    if len(sys.argv) > 1:
         val = sys.argv[1]
-        data = {'q': val}
-        res = requests.post('http://0.0.0.0:5000/search_user', data)
-        if json.load(res.text):
-            res.raise_for_status()
-        print(res.text)
-    except json.decoder.JSONDecodeError as err:
+    dat = {'q': val}
+    res = requests.post('http://0.0.0.0:5000/search_user', data)
+    try:
+        r = res.json()
+        if r == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+    except ValueError:
         print("Not a valid JSON")
-    except IndexError:
-        print("No result")
