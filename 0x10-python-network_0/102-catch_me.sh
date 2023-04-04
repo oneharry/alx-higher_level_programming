ed476d53933c100296b03700d20a7f4da90cc685
oneharry <harryofficialchidi@yahoo.com>
2023-04-04 22:33:35 +0000

    fixed errs

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends request that causes server respond with msg: "You got me"
-curl -X -sL -d "Origin=You got me!" "0.0.0.0:5000/catch_me"
+curl -sXL PUT -H "Origin:You got me!" "0.0.0.0:5000/catch_me"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Post to an arg URL and displays the response body
-curl POST -sX -d "email=test@gmail.com&subject=I will always be here for PLD"
-"$1"
+curl -sX POST --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"
