6f9d33cf142bb8aeb8d493a1f371fe9ef61cc021
oneharry <harryofficialchidi@yahoo.com>
2023-04-04 22:39:54 +0000

    fixed err

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends request that causes server respond with msg: "You got me"
-curl -sXL PUT -H "Origin:You got me!" "0.0.0.0:5000/catch_me"
+curl -sXL PUT -d "user_id=98" -H "Origin:You got me!" "0.0.0.0:5000/catch_me"
