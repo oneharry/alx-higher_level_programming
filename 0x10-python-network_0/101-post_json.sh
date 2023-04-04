3bfd624ddab82e55a14cd5286a18503780ea7c06
oneharry <harryofficialchidi@yahoo.com>
2023-04-04 22:37:59 +0000

    fixed errs

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends request to POST to URL and display the body of response
-curl -sX POST -d "@$2" -H "Content_Type: application/json" "$1"
+curl -sX POST -d "@$2" -H "Content-Type: application/json" "$1"
