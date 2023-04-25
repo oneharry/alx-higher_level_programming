27e4a6ee044f482112fc65e051d47b49dd0a913f
oneharry <harryofficialchidi@yahoo.com>
2023-04-25 22:23:46 +0000

    task 8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const arg = process.argv;
+if (arg.length > 1) {
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + arg[2];
+  request(url, (err, res, body) => {
+    if (err) {
+      return;
+    }
+    const charas = JSON.parse(body).characters;
+
+    for (let i = 0; i < charas.length; i++) {
+      ch = charas[i];
+      request(ch, (err, res, body) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        const char = JSON.parse(body).name;
+        console.log(char);
+      });
+    }
+  });
+}
