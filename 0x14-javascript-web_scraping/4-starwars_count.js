3aabf62f3c8ad74ff721f7838cc405d658740e50
oneharry <harryofficialchidi@yahoo.com>
2023-04-25 17:39:04 +0000

    star wars: number of films for character ID 18

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const arg = process.argv;
+let num;
+let films;
+let cha;
+if (arg.length) {
+  const url = arg[2];
+  request(url, (err, res, body) => {
+    if (err) {
+      return;
+    }
+    films = JSON.parse(body).results;
+    num = films.filter(film => {
+      cha = film.characters;
+      return cha.includes('https://swapi-api.alx-tools.com/api/people/18/');
+    });
+    console.log(num.length);
+  });
+}
